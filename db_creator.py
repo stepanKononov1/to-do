2339c00873724aa2d15eb3ef65b6bb7b53851e80
home <stepan.kot.67@mail.ru>
2024-04-07 16:43:03 +0300

    ...

--- db_creator.py
+++ db_creator.py
@@ -14,20 +14,31 @@ class Database:
     ProjectID integer,
     PRIMARY KEY(TaskID),
     FOREIGN KEY(ProjectID) REFERENCES Projects(ProjectID)
-    );"""
-
-    cursor.execute(sql)
-    sql = """
-    CREATE TABLE IF NOT EXISTS Projects (
-    ProjectID integer,
-    Description text,
-    Deadline date,
-    Created timestamp,
-    Completed timestamp,
-    PRIMARY KEY(ProjectID)
-    );"""
-    cursor.execute(sql)
+    );
+    CREATE TABLE IF NOT EXISTS Edu_deadlines (
+    pk INTEGER NOT NULL UNIQUE,
+    course INTEGER,
+    semester INTEGER,
+    IC_semester_deadline TEXT,
+    PRIMARY KEY (pk AUTOINCREMENT)
+    );
+    CREATE TABLE IF NOT EXISTS Edu_semester_data (
+    id INTEGER NOT NULL UNIQUE,
+    Edu_deadline_pk	INTEGER,
+    subject	TEXT,
+    max_hours INTEGER,
+    PRIMARY KEY(id AUTOINCREMENT)
+    );
+    CREATE TABLE Edu_subjects (
+    pk INTEGER,
+    subject TEXT,
+    topic TEXT UNIQUE,
+    hours INTEGER,
+    PRIMARY KEY(pk AUTOINCREMENT)
+    );
+    """
     connection.close()
 
 
 db = Database()
+
