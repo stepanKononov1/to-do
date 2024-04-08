9a1ba7e80e7a8380d5e0a37dee9795faede9fe8f
stepanKononov1 <94189109+stepanKononov1@users.noreply.github.com>
2024-04-08 18:54:00 +0300

    Add files via upload

--- sorting_feature.py
+++ sorting_feature.py
@@ -31,25 +31,36 @@ for deadline in deadlines:
     weeks_total = len(periods)
     load = db.get_all_sum_hours_semester(semester)
     avg_load_per_period = int(load / weeks_total) + 1
-    print(avg_load_per_period, ' avg')
+    print('avg per period: ', avg_load_per_period)
     period_topics = []
     for subject in req_subjects:
         subject_name = subject[0]
         topics = db.get_topics(subject_name)
         topics_total = len(topics)
-        topics_density = int(topics_total / weeks_total) + 1
+        topics_abs_float = round(topics_total / weeks_total, 1)
+        topics_density = int(topics_abs_float)
+        topics_exception = int(1 / (topics_abs_float % 1))
         for period in periods:
-            for _ in range(topics_density):
+            count = periods.index(period)
+            if count % topics_exception == 0:
                 try:
-                    topic = topics.pop()
-                    period_topics.append([period, *topic])
+                    topic = topics.pop(0)
+                    period_topics.append([period.date(), *topic])
                 except IndexError:
                     break
-    for period in periods:
-        total_hours = 0
-        for period_topic in period_topics:
-            deadline, max_hours = period_topic[0], period_topic[3]
-            if deadline == period:
-                total_hours += max_hours
-        print(total_hours)
+            for _ in range(topics_density):
+                try:
+                    topic = topics.pop(0)
+                    period_topics.append([period.date(), *topic])
+                except IndexError:
+                    break
+    # for period in periods:
+    #     total_hours = 0
+    #     for period_topic in period_topics:
+    #         deadline, max_hours = period_topic[0], period_topic[3]
+    #         if deadline == period.date():
+    #             total_hours += max_hours
+    #     print(total_hours)
+    period_topics.sort(key=lambda x: x[0])
+    [print(period) for period in period_topics]
     date_start = deadline
